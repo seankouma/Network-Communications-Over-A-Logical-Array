28a23feb8403ad3b83a0b65179982230b1c786dc
Nick Kowalczyk <work.kowalczyk@gmail.com>
2022-02-01 11:47:25 -0700

    marhsaling in registry response

--- hw1/src/cs455/overlay/wireformats/RegisterResponse.java
+++ hw1/src/cs455/overlay/wireformats/RegisterResponse.java
@@ -2,12 +2,16 @@ package cs455.overlay.wireformats;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 
 public class RegisterResponse implements Protocol, Event {
     int messageType = Protocol.REGISTER_RESPONSE;
     public int identifier = 0;
+    public byte status;
 
     public RegisterResponse(byte[] marshalledBytes) throws IOException {
         ByteArrayInputStream baInputStream = new ByteArrayInputStream(marshalledBytes);
@@ -19,13 +23,31 @@ public class RegisterResponse implements Protocol, Event {
 
     @Override
     public byte[] getBytes() throws IOException {
-        // TODO Auto-generated method stub
-        return null;
+        byte[] marshalledBytes = null;
+        ByteArrayOutputStream byteArrOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dout =
+        new DataOutputStream(new BufferedOutputStream(byteArrOutputStream));
+        dout.writeInt(messageType);
+        dout.writeInt(identifier);
+        dout.writeByte(status);
+        dout.flush();
+        marshalledBytes = byteArrOutputStream.toByteArray();
+        byteArrOutputStream.close();
+        dout.close();
+        return marshalledBytes;
     }
 
     @Override
     public int getType() {
         return messageType;
+    }
+
+    public byte getStatus(){
+        return status;
+    }
+
+    public int getIdentifier(){
+        return identifier;
     }
     
 }
